56341421f6073e9c56bc0bd71fc73b910228c5ed
cortxu <cortxu@tencent.com>
2020-07-12 21:15:29 +0800

    update

--- x264_config.h
+++ x264_config.h
@@ -2,7 +2,7 @@
 #define X264_INTERLACED    1
 #define X264_BIT_DEPTH     0
 #define X264_CHROMA_FORMAT 0
-#define X264_REV 2
+#define X264_REV 3
 #define X264_REV_DIFF 0
-#define X264_VERSION " r2 e57d8bb"
-#define X264_POINTVER "0.158.2 e57d8bb"
+#define X264_VERSION " r3 ba9bcba"
+#define X264_POINTVER "0.158.3 ba9bcba"
